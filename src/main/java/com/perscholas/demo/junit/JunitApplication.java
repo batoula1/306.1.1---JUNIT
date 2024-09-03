a6627f613eed67dfa54058f752043acd46c76aa1
batoula1 <102437354+batoula1@users.noreply.github.com>
2024-09-02 20:37:01 -0400

    done

--- src/main/java/com/perscholas/demo/junit/JunitApplication.java
+++ src/main/java/com/perscholas/demo/junit/JunitApplication.java
@@ -3,7 +3,6 @@ package com.perscholas.demo.junit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class JunitApplication {
 
 	public static void main(String[] args) {
